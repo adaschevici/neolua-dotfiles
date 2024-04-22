12d61a9da9b558e2ba5fdf9d2bd927a9ba719e9d
Artur Das <a.daschevici@gmail.com>
2024-04-22 17:16:48 +0200

    Add lsp for error highlighting

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,18 @@
+return {
+  'VonHeikemen/lsp-zero.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    {'neovim/nvim-lspconfig'},
+    { -- Optional
+      'williamboman/mason.nvim',
+      build = function()
+        -- Encapsulate the pcall invocation within a function definition
+        pcall(vim.cmd, 'MasonUpdate')
+      end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    {'hrsh7th/nvim-cmp'},                  -- Required
+    {'hrsh7th/cmp-nvim-lsp'},              -- Required
+    {'L3MON4D3/LuaSnip'},                  -- Required
+  },
+}
