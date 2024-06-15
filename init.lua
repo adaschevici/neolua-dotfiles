ee5ac622a39c460693c9a2b51ed269d52c438f05
Artur Das <a.daschevici@gmail.com>
2024-06-16 00:10:03 +0200

    Update init lua with line numbering

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 -- This file simply bootstraps the installation of Lazy.nvim and then calls other files for execution
 -- This file doesn't necessarily need to be touched, BE CAUTIOUS editing this file and proceed at your own risk.
 
+-- Enable line numbers
+vim.opt.number = true
+
+-- Enable relative line numbers
+vim.opt.relativenumber = true
+
 vim.api.nvim_command("cnoreabbrev W w")
 vim.api.nvim_command("cnoreabbrev Q q")
 vim.api.nvim_command("cnoreabbrev Qa qa")
@@ -40,12 +46,6 @@ if not pcall(require, "lazy") then
   vim.cmd.quit()
 end
 
--- vim.api.nvim_exec([[
---   augroup FormatAutogroup
---     autocmd!
---     autocmd BufWritePost *.py,*.js,*.mjs,*.ts,*.css,*.html,*.rs,*.go FormatWrite
---   augroup END
--- ]], true)
 
 local group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
 vim.api.nvim_create_autocmd("BufWritePost", {
@@ -66,80 +66,5 @@ vim.api.nvim_create_autocmd("FileType", {
   group = spellcheck_group,
 })
 
--- Function to move to the next spelling error
-_G.next_spell_error = function()
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(']s', true, true, true), 'n', true)
-  -- vim.api.nvim_command(']s')
-end
-
--- Function to move to the previous spelling error
-_G.prev_spell_error = function()
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('[s', true, true, true), 'n', true)
-  -- vim.api.nvim_command('[s')
-end
-
--- Key mappings for navigating spelling errors
-vim.api.nvim_set_keymap('n', '<leader>sn', ':lua next_spell_error()<CR>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>sp', ':lua prev_spell_error()<CR>', { noremap = true, silent = true })
-
--- local function populate_spell_check_errors()
---   local qf_list = {}
---   local bufnr = vim.api.nvim_get_current_buf()
---   local line_count = vim.api.nvim_buf_line_count(bufnr)
---
---   for lnum = 1, line_count do
---     local line = vim.api.nvim_buf_get_lines(bufnr, lnum - 1, lnum, false)[1]
---     local col = 0
---
---     while col < #line do
---       local start, end_col = vim.fn.spellbadword(line:sub(col + 1))
---       if start ~= "" then
---         table.insert(qf_list, {
---           bufnr = bufnr,
---           lnum = lnum,
---           col = col + 1,
---           text = 'Spelling error: ' .. line:sub(col + start, col + end_col - 1)
---         })
---         col = col + end_col
---       else
---         break
---       end
---     end
---   end
---
---   if next(qf_list) ~= nil then
---     vim.fn.setqflist(qf_list, 'r')
---   else
---     print("No spelling errors found")
---   end
--- end
--- local function populate_spell_check_errors()
---   local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
---   local qf_list = {}
---
---   for lnum, line in ipairs(lines) do
---     for _, word in ipairs(vim.fn.split(line, '\\W\\+')) do
---       if vim.fn.spellbadword(word)[1] ~= '' then
---         table.insert(qf_list, {
---           bufnr = vim.api.nvim_get_current_buf(),
---           lnum = lnum,
---           col = vim.fn.match(line, '\\<' .. word .. '\\>') + 1,
---           text = 'Spelling error: ' .. word
---         })
---       end
---     end
---   end
---
---   vim.fn.setqflist(qf_list, 'r')
--- end
---
--- Function to open the quickfix list
--- local function open_quickfix()
---   vim.api.nvim_command('copen')
--- end
---
--- -- Populate quickfix list with spell check errors and open it
--- populate_spell_check_errors()
--- open_quickfix()
 
 require "lazy_setup"
