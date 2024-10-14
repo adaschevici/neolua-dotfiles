cd53a93f34e232ca770778f1b7e53a16a35efcd3
Artur Das <a.daschevici@gmail.com>
2024-10-14 23:14:49 +0200

    Remove useless comments

--- init.lua
+++ init.lua
@@ -42,55 +42,5 @@ if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
 end
 
 vim.opt.rtp:prepend(lazypath)
---
--- -- validate that lazy is available
--- if not pcall(require, "lazy") then
---   -- stylua: ignore
---   vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
--- 	vim.fn.getchar()
--- 	vim.cmd.quit()
--- end
---
--- local group = vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
---
--- vim.api.nvim_create_autocmd("BufWritePost", {
--- 	pattern = { "*.py", "*.js", "*.mjs", "*.ts", "*.css", "*.html", "*.rs", "*.go" },
--- 	command = "FormatWrite",
--- 	group = group,
--- })
---
--- local spellcheck_group = vim.api.nvim_create_augroup("SpellCheckMarkdown", { clear = true })
---
--- -- Create an autocommand to enable spellcheck for markdown files
--- vim.api.nvim_create_autocmd("FileType", {
--- 	pattern = "markdown",
--- 	callback = function()
--- 		vim.opt_local.spell = true
--- 		vim.opt_local.spelllang = "en_us" -- You can set your preferred language here
--- 	end,
--- 	group = spellcheck_group,
--- })
 
 require("lazy_setup")
-
--- local function is_large_file(bufnr)
--- 	local file_size_mb = vim.fn.getfsize(vim.api.nvim_buf_get_name(bufnr)) / 1024 / 1024
--- 	return file_size_mb > 0.5
--- end
---
--- vim.api.nvim_create_autocmd({ "BufReadPost" }, {
--- 	pattern = "*",
--- 	callback = function(args)
--- 		if is_large_file(args.buf) then
--- 			vim.cmd([[
---         syntax off
---         setlocal foldmethod=manual
---         TSDisable highlight
---         LSPDisable  -- Disable Language Server Protocol features
---         setlocal nocursorline nocursorcolumn -- Disable extras that might cause slowdowns
---         setlocal spell spelllang=en_us -- Keep spellcheck but simplify
---       ]])
--- 		end
--- 	end,
--- })
--- vim.api.nvim_set_keymap("n", "<leader>fw", "<cmd>Telescope grep_string<cr>", { noremap = true, silent = true })
